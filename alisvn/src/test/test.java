daa2cf282080cf40b4a5a139bb6b3cf513193d44
Amon <248779716@qq.com>
2018-08-08 16:23:01 +0800

    提交

---
+++ alisvn/src/test/test.java
@@ -0,0 +1,14 @@
+package test;
+/**
+* @author Amon
+* @version 创建时间：2018年8月8日 下午4:07:24
+* 类说明
+*/
+public class test {
+
+	public static void main(String[] args) {
+	System.out.println("alisvn测试");
+
+	}
+
+}
